daf17d85bbaa9dabbc9a71495a2f33e72f161d79
jason <jason@zgwit.com>
2025-02-26 01:49:26 +0800

    优化job

--- cron/cron.go
+++ cron/cron.go
@@ -16,16 +16,18 @@ func init() {
 	}
 }
 
-func Interval(interval int64, fn func()) (gocron.Job, error) {
-	return scheduler.NewJob(
+func Interval(interval int64, fn func()) (*Job, error) {
+	job, err := scheduler.NewJob(
 		gocron.DurationJob(time.Second*time.Duration(interval)),
 		gocron.NewTask(func() { _ = pool.Insert(fn) }),
 	)
+	return &Job{job}, err
 }
 
-func Crontab(crontab string, fn func()) (gocron.Job, error) {
-	return scheduler.NewJob(
+func Crontab(crontab string, fn func()) (*Job, error) {
+	job, err := scheduler.NewJob(
 		gocron.CronJob(crontab, false),
 		gocron.NewTask(func() { _ = pool.Insert(fn) }),
 	)
+	return &Job{job}, err
 }
