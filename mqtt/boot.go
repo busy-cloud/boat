2811bf9e9a2657dcc11be083dea4891e61af6ecd
jason <jason@zgwit.com>
2025-03-23 22:15:58 +0800

    bug，pool需要提前启动

--- mqtt/boot.go
+++ mqtt/boot.go
@@ -6,6 +6,6 @@ func init() {
 	boot.Register("mqtt", &boot.Task{
 		Startup:  Startup,
 		Shutdown: Shutdown,
-		Depends:  []string{"config", "broker"},
+		Depends:  []string{"config", "broker", "pool"},
 	})
 }
