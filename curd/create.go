1d98215957e206ba024a128ebd99194c2b50d97d
jason <jason@zgwit.com>
2025-03-24 16:32:03 +0800

    生成默认值

--- curd/create.go
+++ curd/create.go
@@ -4,6 +4,8 @@ import (
 	"github.com/busy-cloud/boat/api"
 	"github.com/busy-cloud/boat/db"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"reflect"
 )
 
 func ApiCreate[T any]() gin.HandlerFunc {
@@ -13,6 +15,15 @@ func ApiCreate[T any]() gin.HandlerFunc {
 		if err != nil {
 			api.Error(ctx, err)
 			return
+		}
+
+		//默认值
+		field := reflect.ValueOf(data).Elem().FieldByName("Id")
+		if !field.IsZero() && field.Kind() == reflect.String {
+			if field.Len() == 0 {
+				key := xid.New().String()
+				field.SetString(key)
+			}
 		}
 
 		_, err = db.Engine().InsertOne(&data)
@@ -32,6 +43,15 @@ func ApiCreateHook[T any](before, after func(m *T) error) gin.HandlerFunc {
 		if err != nil {
 			api.Error(ctx, err)
 			return
+		}
+
+		//默认值
+		field := reflect.ValueOf(data).Elem().FieldByName("Id")
+		if !field.IsZero() && field.Kind() == reflect.String {
+			if field.Len() == 0 {
+				key := xid.New().String()
+				field.SetString(key)
+			}
 		}
 
 		if before != nil {
