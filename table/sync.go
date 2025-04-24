61bc7b3890d7bf9d8d1df27dbd66abba7df82daa
jason <jason@zgwit.com>
2025-04-24 19:56:16 +0800

    小改

--- table/sync.go
+++ table/sync.go
@@ -7,24 +7,26 @@ import (
 )
 
 func Sync(tables []*Table) error {
+	//此函数仿照xorm.Engine.Sync
 
-	tbs, err := db.Engine().Dialect().GetTables(db.Engine().DB(), context.Background())
+	//每次把所有表都查出来了
+	originTables, err := db.Engine().Dialect().GetTables(db.Engine().DB(), context.Background())
 	if err != nil {
 		return err
 	}
 
 	for _, table := range tables {
 
-		var t *schemas.Table
-		for _, tb := range tbs {
-			if tb.Name == table.Name {
-				t = tb
+		var originTable *schemas.Table
+		for _, ot := range originTables {
+			if ot.Name == table.Name {
+				originTable = ot
 				break
 			}
 		}
 
 		//新表
-		if t == nil {
+		if originTable == nil {
 			err = table.Create()
 			if err != nil {
 				return err
@@ -36,8 +38,8 @@ func Sync(tables []*Table) error {
 		schema := table.Schema()
 		for _, col := range schema.Columns() {
 			found := false
-			for _, c2 := range t.Columns() {
-				if col.Name == c2.Name {
+			for _, oc := range originTable.Columns() {
+				if col.Name == oc.Name {
 					found = true
 					break
 				}
@@ -45,8 +47,8 @@ func Sync(tables []*Table) error {
 
 			//添加新列
 			if !found {
-				t.AddColumn(col)
-				sql := db.Engine().Dialect().AddColumnSQL(t.Name, col)
+				originTable.AddColumn(col)
+				sql := db.Engine().Dialect().AddColumnSQL(originTable.Name, col)
 				_, err = db.Engine().Exec(sql)
 				if err != nil {
 					return err
@@ -59,8 +61,8 @@ func Sync(tables []*Table) error {
 		//更新索引
 		for _, index := range schema.Indexes {
 			found := false
-			for _, i := range t.Indexes {
-				if index.Equal(i) {
+			for _, oi := range originTable.Indexes {
+				if index.Equal(oi) {
 					found = true
 					break
 				}
@@ -68,14 +70,14 @@ func Sync(tables []*Table) error {
 
 			//添加索引
 			if !found {
-				sql := db.Engine().Dialect().CreateIndexSQL(t.Name, index)
+				sql := db.Engine().Dialect().CreateIndexSQL(originTable.Name, index)
 				_, err = db.Engine().Exec(sql)
 				if err != nil {
 					return err
 				}
 			}
 
-			//TODO 删除索引
+			//TODO 删除多余索引
 		}
 
 	}
